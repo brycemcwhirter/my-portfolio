7cb9ed0f6da2ce0a21317de36c8c54b715fb4bc7
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-07-29 06:32:44 -0500

    First Commit on Portfolio Website w/ Svelte

---
+++ src/components/Main.svelte
@@ -0,0 +1,58 @@
+<script>
+</script>
+
+<main>
+	<section id="introPage">
+		<div class="flex min-h-screen flex-col items-center justify-center border">
+			<h1 class="text-2xl font-semibold">Bryce McWhirter</h1>
+			<p class="font-medium">Full Stack Developer</p>
+			<p class="text-sm font-medium">DFW</p>
+		</div>
+	</section>
+	<section id="aboutPage">
+		<div class="flex min-h-screen flex-col items-center justify-center space-y-6 border sm:p-px">
+			<h1 class="text-2xl font-semibold">About Me</h1>
+			<p class="m-4 text-center text-xs sm:text-sm lg:m-32">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula viverra ex, ut
+				tincidunt lacus laoreet vel. Praesent vel aliquam metus. Vestibulum id turpis eget ligula
+				congue aliquam in a metus. Sed tortor felis, suscipit sit amet nulla eu, feugiat ultricies
+				justo. In rutrum lacus vitae mauris ultricies, non tempor sapien consectetur. Nunc id
+				convallis leo. Aliquam non ligula ultrices, placerat nisl in, mollis arcu. Vivamus convallis
+				dignissim massa, at gravida ante. Ut ac neque justo. Aliquam non ligula ultrices, placerat
+				nisl in, mollis arcu. Vivamus convallis dignissim massa, at gravida ante. Ut ac neque justo.
+				Aliquam non ligula ultrices, placerat nisl in, mollis arcu. Vivamus convallis.
+			</p>
+		</div>
+	</section>
+
+	<section id="resumePage">
+		<div class="border">Resumé</div>
+	</section>
+	<section id="projectPage">
+		<div class="border">Project</div>
+	</section>
+	<section id="contactPage">
+		<div class="flex min-h-screen flex-col items-center justify-center space-y-6 border sm:p-px">
+			<h1 class="text-2xl font-semibold">Contact</h1>
+			<p class="m-4 text-center text-xs sm:text-sm lg:m-32">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula viverra ex, ut
+				tincidunt lacus laoreet vel.
+			</p>
+			<div class="flex-row space-x-3">
+				<a
+					href="https://www.linkedin.com/in/bryce-mcwhirter-5b9330146/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i class="fa-brands fa-linkedin-in"></i>
+				</a>
+				<a href="https://github.com/brycemcwhirter" target="_blank" rel="noopener noreferrer">
+					<i class="fa-brands fa-github"></i>
+				</a>
+				<a href="mailto:mcwhirterbryce@gmail.com" target="_blank" rel="noopener noreferrer">
+					<i class="fa fa-envelope" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div>
+	</section>
+</main>
