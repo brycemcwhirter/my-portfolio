e4449f53585ea6fdf70e9e8e712dc438bc29b735
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-02 06:40:51 -0500

    Work on resume

--- src/components/ExperienceCard.svelte
+++ src/components/ExperienceCard.svelte
@@ -11,17 +11,22 @@
 
 <!-- todo Align Text to Center if on a small screen-->
 {#each experiences as experience}
-	<div class="gap-1 space-y-1 p-4 text-left">
-		<h2 class="text-md font-bold md:text-xl lg:text-2xl">{experience.title}</h2>
+	<div class="gap-1 p-5 text-left md:p-10">
+		<h2 class="text-md font-bold md:text-xl lg:text-2xl">
+			{experience.title}
+		</h2>
+
 		<a href={experience.companyLink} target="_blank">
-			<p class="pad-1 text-sm font-medium italic hover:underline md:text-base lg:text-lg">
+			<h4 class="pad-8 text-md font-medium italic hover:underline md:text-lg">
 				{experience.company}
-			</p>
+			</h4>
 		</a>
+		<p class="pad-1 text-xs font-medium italic lg:text-lg">{experience.dateRange}</p>
+
 		<p class="text-xs font-light lg:text-base">{experience.description}</p>
 
-		{#each experience.bulletPoints as bulletPoint}
+		<!-- {#each experience.bulletPoints as bulletPoint}
 			<p class="text-xs font-light lg:text-base">•{bulletPoint}</p>
-		{/each}
+		{/each} -->
 	</div>
 {/each}
--- src/components/Resume.svelte
+++ src/components/Resume.svelte
@@ -11,40 +11,41 @@
 	<div class="grid grid-cols-1 gap-y-9 lg:grid-cols-2 lg:space-x-4">
 		<div id="codingLanguages" class="m-auto grid grid-cols-1 items-center space-y-6 p-3">
 			<h1 class="flex justify-center text-xl font-semibold md:text-2xl lg:text-3xl">
-				Coding Languages
+				Technology Skills
 			</h1>
 			<div
 				class="grid grid-cols-4 items-center justify-items-center gap-x-14 gap-y-4 md:gap-x-28 md:gap-y-9"
 			>
+				<!--TODO svelte icon!-->
 				<!-- Icons to show on small screens -->
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-2x md:hidden"></i>
+				<i class="fa-brands fa-svelte fa-2x md:hidden"></i>
+				<i class="fa-brands fa-react fa-2x md:hidden"></i>
+				<i class="fa-brands fa-vuejs fa-2x md:hidden"></i>
+				<i class="fa-brands fa-php fa-2x md:hidden"></i>
+				<i class="fa-brands fa-python fa-2x md:hidden"></i>
+				<i class="fa-brands fa-docker fa-2x md:hidden"></i>
+				<i class="fa-brands fa-java fa-2x md:hidden"></i>
+				<i class="fa-brands fa-c fa-2x md:hidden"></i>
 
 				<!-- Icons to show on medium screens -->
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
-				<i class="fa-brands fa-linkedin-in fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-svelte fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-react fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-vuejs fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-php fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-python fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-docker fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-java fa-3x hidden md:inline lg:hidden"></i>
+				<i class="fa-brands fa-c fa-3x hidden md:inline lg:hidden"></i>
 
 				<!-- Icons to show on large screens -->
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
-				<i class="fa-brands fa-linkedin-in fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-svelte fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-react fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-vuejs fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-php fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-python fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-docker fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-java fa-5x hidden lg:inline"></i>
+				<i class="fa-brands fa-c fa-5x hidden lg:inline"></i>
 			</div>
 		</div>
 
@@ -55,9 +56,32 @@
 			<h1 class="align-center flex justify-center text-xl font-semibold md:text-2xl lg:text-3xl">
 				Resumé
 			</h1>
+
+			<h2 class="align-center text-md flex justify-center font-medium md:text-xl lg:text-2xl">
+				Education
+			</h2>
+
+			<div class="p-5 md:p-10">
+				<p class="text-md font-bold md:text-xl lg:text-2xl">
+					Bachelor of Science in Computer Science
+				</p>
+				<p class="text-md font-medium italic md:text-lg">Baylor University</p>
+				<p class="pad-1 text-xs font-medium italic lg:text-lg">2023</p>
+			</div>
+
+			<h2 class="align-center flex justify-center text-lg font-medium md:text-xl lg:text-2xl">
+				Professional Experience
+			</h2>
+
 			<ExperienceCard />
+			<!--todo Download Resumé Button-->
+			<div class="flex items-center justify-center">
+				<a
+					href="/Gene Bryce McWhirter Resume.pdf"
+					class="border px-4 py-2 hover:bg-white hover:text-black"
+					target="_blank">Download Resumé Here</a
+				>
+			</div>
 		</div>
 	</div>
-
-	<!--todo Download Resumé Button-->
 </div>
