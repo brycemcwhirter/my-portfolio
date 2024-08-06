74235075574ef146c4af3c672207a8e22c54d854
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-08-06 06:33:00 -0500

    project updates

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -9,16 +9,6 @@
 	});
 </script>
 
-<!-- {#each projects as project}
-	<div class="gap-1 space-y-1 p-4 text-center md:text-left">
-		<h2 class="text-md font-bold md:text-xl">{project.title}</h2>
-		<a href={project.link} target="_blank">
-			<p class="pad-1 text-sm font-medium italic hover:underline md:text-base">link</p>
-		</a>
-		<p class="text-xs font-light">{project.description}</p>
-	</div>
-{/each} -->
-
 {#each projects as project}
 	<div class="mx-auto max-w-lg p-10">
 		<div class="mb-5 max-w-sm rounded-lg border border-gray-800">
